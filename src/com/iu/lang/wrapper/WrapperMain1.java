dc9302bb6621f08e4cadf05d378e973d6beb3a1a
kdy991101 <kdy991101@naver.com>
2022-07-19 17:33:40 +0900

    미완성

---
+++ src/com/iu/lang/wrapper/WrapperMain1.java
@@ -0,0 +1,68 @@
+package com.iu.lang.wrapper;
+
+import com.iu.lang.wrapper.ex1.WrapperEx1;
+import com.iu.lang.wrapper.ex1.WrapperEx2;
+
+public class WrapperMain1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Integer.BYTES); //결과 4바이트
+		System.out.println(Integer.MAX_VALUE);//결과 21억
+		System.out.println(Integer.MIN_VALUE);//결과 -21억
+		System.out.println(Integer.SIZE);//결과 32비트
+		
+		String num = "10";
+		
+		Integer integer = new Integer(num);
+		int n = integer.intValue();
+		
+		n = 50;
+		integer = new Integer(n);
+		//integer = 참조변수, int = 정수
+		//원래는 불가능함
+		//하지만 가능하게 만들어놈..?
+		integer = n;//auto-boxing 정수의 값 인트를 레퍼런스로 바꿔줌
+		
+		n = integer;//auto-unboxing 
+		
+		long l = 10l;
+//		integer = l;//오토박싱 불가
+		long ll = 1l;
+		System.out.println(ll);
+		
+//		같은 형식끼리만 가능함
+	
+		integer = (int)l;
+		integer = (int)ll;
+		
+		System.out.println(n);
+		
+		//중요
+		//문자열 중에서 숫자형식만 가능한 것임
+		String num1 = "50";
+		String num2 = "3.12";
+		String num3 = "60";	
+//	문자열 -> primitive
+		
+		int n1 = Integer.parseInt(num1);
+		Double n2 = Double.parseDouble(num2);
+		long n3 = Long.parseLong(num3);
+		System.out.println(n1*2);
+		System.out.println(n2*2);
+		System.out.println(n3*2);
+		
+		
+		
+		
+//		System.out.println(num + 2);
+		
+		WrapperEx1 we = new WrapperEx1();
+//		we.ex1();
+		WrapperEx2 we2 = new WrapperEx2();
+		we2.juminCheck();
+
+	}
+
+}
---
+++ src/com/iu/lang/wrapper/ex1/WrapperEx2.java
@@ -0,0 +1,50 @@
+package com.iu.lang.wrapper.ex1;
+
+import java.util.Scanner;
+
+public class WrapperEx2 {
+	
+	//juminCheck
+	//주민번호 입력
+	//9  7  1 1 2   4 - 1 2  3  4  5  6 7
+  //* 2  3  4 5 6   7 - 8 9  2  3  4  5 6 
+//   18 21  4 5 12 28 - 8 18 6 12 20 30
+	//192
+	//총합을 11로 나누어 나머지를 구함
+	//192/11 몫-17 나머지 -5
+	//나머지를 11에서 뺸 결과 (6)
+	
+	//나머지를 11에서 뺀 결과를 체크용 번호와 같은지 비교
+//	11-0 = 11
+	
+	//만약에 위의 연상결과가 두자리라면 연산결과를 다시 10으로 나눈 나머지와
+	//체크용번호를 비교
+	//11/10=1
+	public void juminCheck() {
+		String jumin = "9 9 1 1 0 1 - 1 2 3 4 5 6 7";
+//		Scanner sc  = new Scanner(System.in);
+//		System.out.println("주민번호를 입력하여 주세요.");
+//		String number = sc.next();
+//		String str2 = str.replace('l', 'w');
+		String ju = jumin.replace("-", "");
+		System.out.println(ju);
+		//결과 : 9 9 1 1 0 1  1 2 3 4 5 6 7
+		char jumin2[] = ju.toCharArray();
+		System.out.println(jumin2);
+		for(int i = 0; i<jumin2.length; i++)
+		{
+			System.out.print(jumin2[i]);
+		}
+//		int[] nums = Character.getNumericValue();
+//		int i;
+//		for(i = 0; i<ju.length(); i++)
+		{
+//			jumin2[i];
+		}
+		
+		
+		
+		
+		
+	}
+}
